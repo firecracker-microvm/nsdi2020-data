5a81de535c753ca881d33ca3124be2ceefc24fea
Rolf Neugebauer <rn@rneugeba.io>
2019-01-25 22:17:26 +0000

    boot-time: Add qemu to boot from initrd
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

---
+++ boot-time/qemu-kvm-initrd.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+. ./qemu-kvm-common.sh
+
+qemu-system-x86_64 \
+    ${QEMU_COMMON} \
+    -no-acpi \
+    -machine q35 \
+    -kernel ../img/boot-time-pci-vmlinuz \
+    -initrd ../img/boot-time-initrd.img \
+    -append "init=/init"
+
+# -append "root=/dev/vda init=/init console=ttyS0"
+# -device virtio-net-device,netdev=n0,mac=5a:39:97:06:c0:fd -netdev user,id=n0 \
+
