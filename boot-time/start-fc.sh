6f5e49852a1c6fdddc1f69d59f9712c7124a6918
Rolf Neugebauer <rn@rneugeba.io>
2019-01-26 15:19:44 +0000

    boot-time: Improve firecracker script
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- boot-time/start-fc.sh
+++ boot-time/start-fc.sh
@@ -1,10 +1,35 @@
-#! /bin/sh
+#! /bin/bash
 
-SOCK=./foo.sock
-KERNEL=../img/boot-time-vmlinux
-ROOTFS=../img/boot-time-disk.img
+rand=$RANDOM
+FC=../bin/firecracker
+LOGFILE=./fc-$rand.log
+SOCK=./fc-$rand.sock
 
-../bin/firecracker --api-sock $SOCK &
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -b) shift; FC=$1
+            ;;
+        -k) shift; KERNEL=$1
+            ;;
+        -r) shift; ROOTFS=$1
+            ;;
+        -t) shift; TIMEFILE=$1
+            ;;
+        -l) shift; LOGFILE=$1
+            ;;
+        -s) shift; SOCK=$1
+            ;;
+        -d) DEBUG=yes
+            ;;
+    esac
+    shift
+done
+
+# TODO: Enable serial console output on debug
+
+us_start=$(($(date +%s%N)/1000))
+$FC --api-sock $SOCK 2> ${LOGFILE} &
 FC_PID=$!
 
 # Create VM
@@ -17,7 +42,7 @@ curl -s --unix-socket "$SOCK" -i \
         \"mem_size_mib\": 512,
         \"cpu_template\": \"T2\",
         \"ht_enabled\": true
-    }"  > /dev/null
+    }" > /dev/null
 
 # Set kernel
 curl -s --unix-socket "$SOCK" -i \
@@ -39,7 +64,7 @@ curl -s --unix-socket "$SOCK" -i \
         \"path_on_host\": \"$ROOTFS\",
         \"is_root_device\": true,
         \"is_read_only\": false
-    }"  > /dev/null
+    }" > /dev/null
 
 # start
 curl -s --unix-socket "$SOCK" -i \
@@ -48,7 +73,18 @@ curl -s --unix-socket "$SOCK" -i \
      -H  "Content-Type: application/json" \
          -d "{
         \"action_type\": \"InstanceStart\"
-     }"  > /dev/null
+     }" > /dev/null
 
 wait $FC_PID
+us_end=$(($(date +%s%N)/1000))
+
+fc_time=$(grep -oE '[0-9]+ ms' $LOGFILE | grep -oE '[0-9]+')
+us_time=$(expr $us_end - $us_start)
+
+if [ "x$TIMEFILE" = "x" ]; then
+    echo $us_time $fc_time
+else
+    echo $us_time $fc_time >> $TIMEFILE
+fi
+
 rm ${SOCK}
