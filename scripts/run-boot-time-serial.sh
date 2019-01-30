56cebe620e8ffd3846d616b317274b7b47f6b8bd
Rolf Neugebauer <rn@rneugeba.io>
2019-01-30 23:33:50 +0000

    Add script to generate serial boot time CDF
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

---
+++ scripts/run-boot-time-serial.sh
@@ -0,0 +1,69 @@
+#! /bin/sh
+
+#set -x
+
+ITER=1000
+DIR=../data/
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -i) shift; ITER=$1
+            ;;
+        -d) shift; DIR=$1
+            ;;
+    esac
+    shift
+done
+
+mkdir -p ${DIR}
+
+FC_RES=${DIR}/boot-times-serial-firecracker.dat
+QEMU_RES=${DIR}/boot-times-serial-qemu.dat
+RES=${DIR}/boot-times-serial.csv
+
+rm -f ${FC_RES} ${QEMU_RES} ${RES}
+
+killall firecracker 2> /dev/null
+for i in $(seq ${ITER}); do
+    ./start-fc.sh -b ../bin/firecracker \
+                  -k ../img/boot-time-vmlinux \
+                  -r ../img/boot-time-disk.img \
+                  -t ${FC_RES}
+    sleep 0.4
+    killall firecracker 2> /dev/null
+done
+rm -f *.log
+
+killall qemu-system-x86_64 2> /dev/null
+for i in $(seq ${ITER}); do
+    ./start-qemu.sh -b ../bin/qemu-system-x86_64 \
+                    -k ../img/boot-time-vmlinuz \
+                    -r ../img/boot-time-disk.img \
+                    -t ${QEMU_RES}
+    sleep 0.4
+    killall qemu-system-x86_64 2> /dev/null
+done
+rm -f *.log
+
+# Munch the data
+echo "bootsecs,vmm" > ${RES}
+
+while IFS= read -r l; do
+    # $l is in microseconds
+    s=$(echo "scale=3; $l/1000000" | bc)
+    echo "$s,qemu" >> ${RES}
+done < ${QEMU_RES}
+
+while IFS= read -r l; do
+    # $l is in [microseconds ms]
+    us=$(echo $l | cut -d ' ' -f1)
+    s=$(echo "scale=3; $us/1000000" | bc)
+    echo "$s,firecracker-e2e" >> ${RES}
+done < ${FC_RES}
+
+while IFS= read -r l; do
+    # $l is in [microseconds ms]
+    ms=$(echo $l | cut -d ' ' -f2)
+    s=$(echo "scale=3; $ms/1000" | bc)
+    echo "$s,firecracker" >> ${RES}
+done < ${FC_RES}
