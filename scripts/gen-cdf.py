348b246ca154b93eef4f5e1d0b35ae6fe6ced863
Rolf Neugebauer <rn@rneugeba.io>
2019-02-04 23:14:24 +0000

    scripts: Add python script to generate CDF
    This script takes a boot time file and generates a file
    with the CDF suitable for plotting with gnuplot.
    
    If the data file contains multiple columns it generates
    multiple sections
    
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

---
+++ scripts/gen-cdf.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+
+# A somewhat skanky script to convert the raw data files generated from the
+# boot time scripts into a data file with a CDF usable from gnuplot. A data
+# file which contains multiple columns it will generate a data file with
+# multiple sections.
+
+import sys
+import os
+import collections
+
+if len(sys.argv) != 2:
+    sys.exit("Please provide a file name")
+
+infn = sys.argv[1]
+outfn = os.path.splitext(infn)[0]
+outfn += "-cdf.dat"
+
+print(outfn)
+
+# Data is a list of Counters
+data = []
+num_cols = 0
+
+with open(infn, 'r') as f:
+    for line in f:
+        l = line.strip()
+        if len(l) == 0 or l[0] == "#":
+            continue
+        elems = l.split()
+
+        # create initial data structure (or sanity check)
+        if len(data) == 0:
+            for _ in elems:
+                data.append(collections.Counter())
+            num_cols = len(elems)
+        else:
+            if num_cols != len(elems):
+                sys.exit("Odd data file: %d != %d", len(data), len(elems))
+
+        for i in range(num_cols):
+            n = int(elems[i])
+            data[i][n] += 1
+
+totals = []
+for i in range(num_cols):
+    totals.append(sum(data[i].values()))
+
+with open(outfn, 'w') as f:
+    for i in range(num_cols):
+        first = True
+        s = 0.0
+        for item in sorted(data[i].items()):
+            t = item[0]
+            c = item[1]
+            if first:
+                f.write("%d %f\n" % (t, s))
+                first = False
+            s += c/float(totals[i])
+            f.write("%d %f\n" % (t, s))
+        f.write("\n\n")
