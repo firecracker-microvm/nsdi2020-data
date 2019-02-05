c7592a6c79b2629d4e8bdd85c0cc0f009edba9d9
Rolf Neugebauer <rn@rneugeba.io>
2019-02-05 23:04:00 +0000

    scripts: Add qemu initrd boot times
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-boot-time-serial.sh
+++ scripts/run-boot-time-serial.sh
@@ -7,7 +7,6 @@ DIR=../data/
 CORES=1
 MEM=256
 
-
 while [ $# -gt 0 ]; do
     case $1 in
         -i) shift; ITER=$1
@@ -22,9 +21,10 @@ mkdir -p ${DIR}
 
 FC_RES=${DIR}/boot-times-serial-fc.dat
 QEMU_RES=${DIR}/boot-times-serial-qemu.dat
+QEMU_INITRD_RES=${DIR}/boot-times-serial-qemu-initrd.dat
 RES=${DIR}/boot-times-serial.csv
 
-rm -f ${FC_RES} ${QEMU_RES} ${RES}
+rm -f ${FC_RES} ${QEMU_RES} ${QEMU_INITRD_RES} ${RES}
 
 killall firecracker 2> /dev/null
 for i in $(seq ${ITER}); do
@@ -53,6 +53,20 @@ for i in $(seq ${ITER}); do
 done
 rm -f *.log
 ./gen-cdf.py ${QEMU_RES}
+
+killall qemu-system-x86_64 2> /dev/null
+for i in $(seq ${ITER}); do
+    ./start-qemu.sh -b ../bin/qemu-system-x86_64 \
+                    -k ../img/boot-time-pci-vmlinuz \
+                    -a ../img/boot-time-initrd.img \
+                    -c $CORES \
+                    -m $MEM \
+                    -t ${QEMU_INITRD_RES}
+    sleep 0.4
+    killall qemu-system-x86_64 2> /dev/null
+done
+rm -f *.log
+./gen-cdf.py ${QEMU_INITRD_RES}
 
 
 # Munch the data
