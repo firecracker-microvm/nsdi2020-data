2033e1224600b0b3063719729cbb69d044886c26
Rolf Neugebauer <neugebar@amazon.com>
2019-02-06 14:36:59 +0000

    prep: Use structure for config in config-fc
    This will make it easier to add comand line options.
    
    Signed-off-by: Rolf Neugebauer <neugebar@amazon.com>

--- prep/config-fc/config-fc.go
+++ prep/config-fc/config-fc.go
@@ -7,30 +7,31 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
 	"os"
 )
 
-const machine_json = `{
-    "vcpu_count": 1,
-    "mem_size_mib": 256,
-    "cpu_template": "T2",
-    "ht_enabled": true
-}`
+type Machine struct {
+	VCPUs       int    `json:"vcpu_count,omitempty"`
+	Memory      int    `json:"mem_size_mib,omitempty"`
+	CPUTemplate string `json:"cpu_template,omitempty"`
+	HyperThread bool   `json:"ht_enabled,omitempty"`
+}
 
-const kernel_json = `{
-  "kernel_image_path": "../img/boot-time-vmlinux",
-  "boot_args": "panic=1 pci=off reboot=k tsc=reliable ipv6.disable=1 init=/init quiet 8250.nr_uarts=0"
-}`
+type Kernel struct {
+	ImagePath string `json:"kernel_image_path,omitempty"`
+	BootArgs  string `json:"boot_args,omitempty"`
+}
 
-const drive_json = `{
-  "drive_id": "1",
-  "path_on_host": "../img/boot-time-disk.img",
-  "is_root_device": true,
-  "is_read_only": true
-}`
+type Drive struct {
+	DriveId      string `json:"drive_id,omitempty"`
+	Path         string `json:"path_on_host,omitempty"`
+	IsRootDevice bool   `json:"is_root_device,omitempty"`
+	IsReadOnly   bool   `json:"is_read_only,omitempty"`
+}
 
 const start_json = `{
     "action_type": "InstanceStart"
@@ -66,17 +67,52 @@ func fc_api(client *http.Client, path, body string) error {
 }
 
 func main() {
+
+	machine := Machine{
+		VCPUs:       1,
+		Memory:      256,
+		CPUTemplate: "T2",
+		HyperThread: true,
+	}
+	b, err := json.Marshal(machine)
+	if err != nil {
+		panic(err)
+	}
+	machine_json := string(b)
+
+	kernel := Kernel{
+		ImagePath: "../img/boot-time-vmlinux",
+		BootArgs:  "panic=1 pci=off reboot=k tsc=reliable ipv6.disable=1 init=/init quiet 8250.nr_uarts=0",
+	}
+	b, err = json.Marshal(kernel)
+	if err != nil {
+		panic(err)
+	}
+	kernel_json := string(b)
+
+	drive := Drive{
+		DriveId:      "1",
+		Path:         "../img/boot-time-disk.img",
+		IsRootDevice: true,
+		IsReadOnly:   true,
+	}
+	b, err = json.Marshal(drive)
+	if err != nil {
+		panic(err)
+	}
+	drive_json := string(b)
+
 	client := make_client(os.Args[1])
-	if e := fc_api(client, "machine-config", machine_json); e != nil {
-		panic(e)
+	if err := fc_api(client, "machine-config", machine_json); err != nil {
+		panic(err)
 	}
-	if e := fc_api(client, "drives/1", drive_json); e != nil {
-		panic(e)
+	if err := fc_api(client, "drives/1", drive_json); err != nil {
+		panic(err)
 	}
-	if e := fc_api(client, "boot-source", kernel_json); e != nil {
-		panic(e)
+	if err := fc_api(client, "boot-source", kernel_json); err != nil {
+		panic(err)
 	}
-	if e := fc_api(client, "actions", start_json); e != nil {
-		panic(e)
+	if err := fc_api(client, "actions", start_json); err != nil {
+		panic(err)
 	}
 }
