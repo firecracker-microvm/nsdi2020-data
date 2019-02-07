9681e63fe08c457f83b07ec3151f324bea57313e
Rolf Neugebauer <rn@rneugeba.io>
2019-02-07 23:28:25 +0000

    Use non-PCI kernel for firecracker
    Stick to the simplest kernel. Will add different kernels soon.
    
    Also remove the initrd boot for qemu.
    
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-boot-time-serial.sh
+++ scripts/run-boot-time-serial.sh
@@ -21,15 +21,14 @@ mkdir -p ${DIR}
 
 FC_RES=${DIR}/boot-times-serial-fc.dat
 QEMU_RES=${DIR}/boot-times-serial-qemu.dat
-QEMU_INITRD_RES=${DIR}/boot-times-serial-qemu-initrd.dat
 RES=${DIR}/boot-times-serial.csv
 
-rm -f ${FC_RES} ${QEMU_RES} ${QEMU_INITRD_RES} ${RES}
+rm -f ${FC_RES} ${QEMU_RES} ${RES}
 
 killall firecracker 2> /dev/null
 for i in $(seq ${ITER}); do
     ./start-fc.sh -b ../bin/firecracker \
-                  -k ../img/boot-time-pci-vmlinux \
+                  -k ../img/boot-time-vmlinux \
                   -r ../img/boot-time-disk.img \
                   -c $CORES \
                   -m $MEM \
@@ -53,20 +52,6 @@ for i in $(seq ${ITER}); do
 done
 rm -f *.log
 ./gen-cdf.py ${QEMU_RES}
-
-killall qemu-system-x86_64 2> /dev/null
-for i in $(seq ${ITER}); do
-    ./start-qemu.sh -b ../bin/qemu-system-x86_64 \
-                    -k ../img/boot-time-pci-vmlinuz \
-                    -a ../img/boot-time-initrd.img \
-                    -c $CORES \
-                    -m $MEM \
-                    -t ${QEMU_INITRD_RES}
-    sleep 0.4
-    killall qemu-system-x86_64 2> /dev/null
-done
-rm -f *.log
-./gen-cdf.py ${QEMU_INITRD_RES}
 
 
 # Munch the data
