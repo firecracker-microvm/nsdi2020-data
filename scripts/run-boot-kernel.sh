018d219998ba95f8b11ded3b83958179cc5898af
Rolf Neugebauer <rn@rneugeba.io>
2019-04-20 12:37:07 +0100

    scripts: Adjust script to test boot times of differnent kernels
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-boot-kernel.sh
+++ scripts/run-boot-kernel.sh
@@ -19,37 +19,43 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-mkdir -p ${DIR}
+RAW=${DIR}/raw
+mkdir -p ${RAW}
 
-RES_PCI=${DIR}/boot-times-fc-pci.dat
-RES_LK=${DIR}/boot-times-fc-linuxkit.dat
+PCI_DAT=${RAW}/boot-serial-fc-pci.dat
+PCI_CDF=${DIR}/boot-serial-fc-pci-cdf.dat
 
-rm -f ${RES_PCI} ${RES_LK}
+LK_DAT=${RAW}/boot-serial-fc-linuxkit.dat
+LK_CDF=${DIR}/boot-serial-fc-linuxkit-cdf.dat
+
+rm -f ${PCI_DAT} ${PCI_CDF} ${LK_DAT} ${LK_CDF}
 
 killall firecracker 2> /dev/null
 for i in $(seq ${ITER}); do
+    echo "Firecracker+PCI kernel: $i"
     ./util_start_fc.sh -b ../bin/firecracker \
                   -k ../img/boot-time-pci-vmlinux \
                   -r ../img/boot-time-disk.img \
                   -c $CORES \
                   -m $MEM \
-                  -t ${RES_PCI}
+                  -t ${PCI_DAT}
     sleep 0.4
     killall firecracker 2> /dev/null
 done
 rm -f *.log
-./util_gen_cdf.py ${RES_PCI}
+./util_gen_cdf.py ${PCI_DAT} ${PCI_CDF}
 
 killall firecracker 2> /dev/null
 for i in $(seq ${ITER}); do
+    echo "Firecracker+LinuxKit kernel: $i"
     ./util_start_fc.sh -b ../bin/firecracker \
                   -k ../img/boot-time-linuxkit-vmlinux \
                   -r ../img/boot-time-disk.img \
                   -c $CORES \
                   -m $MEM \
-                  -t ${RES_LK}
+                  -t ${LK_DAT}
     sleep 0.4
     killall firecracker 2> /dev/null
 done
 rm -f *.log
-./util_gen_cdf.py ${RES_LK}
+./util_gen_cdf.py ${LK_DAT} ${LK_CDF}
