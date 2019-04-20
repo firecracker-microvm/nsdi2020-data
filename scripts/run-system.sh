7122ea35d906a11b00ed6e85b11eeab9e6d8c71a
Rolf Neugebauer <rn@rneugeba.io>
2019-04-20 16:12:33 +0100

    scripts: Fix up system gathering script
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-system.sh
+++ scripts/run-system.sh
@@ -10,20 +10,24 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-mkdir -p ${DIR}
 
-date > ${DIR}/sys_date.txt
-cat /proc/cpuinfo > ${DIR}/sys_cpuinfo.txt
-cat /proc/cmdline > ${DIR}/sys_kernel-cmdline.txt
-free > ${DIR}/sys_memory.txt
-cat /proc/meminfo > ${DIR}/sys_meminfo.txt
-numactl --hardware > ${DIR}/sys_numactl.txt
-lstopo-no-graphics -c > ${DIR}/sys_lstopo.txt
-uname -a > ${DIR}/sys_uname.txt
-lsb_release -a > ${DIR}/sys_lsb_release.txt
-dmidecode > ${DIR}/sys_dmidecode.txt
-lspci -vvv > ${DIR}/sys_lspci.txt
-dmesg > ${DIR}/sys_dmesg.txt
+SYS=${DIR}/sys
+mkdir -p ${SYS}
+
+date                     > ${SYS}/sys_date.txt
+cat /proc/cpuinfo        > ${SYS}/sys_cpuinfo.txt
+cat /proc/cmdline        > ${SYS}/sys_kernel-cmdline.txt
+free                     > ${SYS}/sys_memory.txt
+cat /proc/meminfo        > ${SYS}/sys_meminfo.txt
+uname -a                 > ${SYS}/sys_uname.txt
+lsb_release -a           > ${SYS}/sys_lsb_release.txt
+numactl --hardware       > ${SYS}/sys_numactl.txt
+lstopo-no-graphics -c -v > ${SYS}/sys_lstopo.txt
+lspci -xxxx              > ${SYS}/sys_lspci-xxxx.txt
+lspci -vvv -nn           > ${SYS}/sys_lspci.txt
+dmidecode                > ${SYS}/sys_dmidecode.txt
+dmesg                    > ${SYS}/sys_dmesg.txt
 
 ../bin/qemu-system-x86_64 --version > ${DIR}/sys_qemu.txt
-../bin/firecracker --version  > ${DIR}/sys_firecracker.txt
+../bin/firecracker --version        > ${DIR}/sys_firecracker.txt
+../bin/linuxkit version             > ${DIR}/sys_linuxkit.txt
