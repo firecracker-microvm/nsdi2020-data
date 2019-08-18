b54a726d78310bcef697d992715e44aeedcbda78
Rolf Neugebauer <rn@rneugeba.io>
2019-08-18 11:44:54 +0100

    data: Adjust gnuplot files (update plots)
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- data/update-plots.sh
+++ data/update-plots.sh
@@ -5,6 +5,5 @@ DIRS="m5d.metal-ht m5d.metal rn.haswell rn.nehalem"
 for DIR in $DIRS; do
     gnuplot -e "base='$DIR'" gnuplot/boot-serial-all.gpl
     gnuplot -e "base='$DIR'" gnuplot/boot-serial.gpl
-    gnuplot -e "base='$DIR'" gnuplot/boot-parallel-all.gpl
     gnuplot -e "base='$DIR'" gnuplot/boot-parallel.gpl
 done
