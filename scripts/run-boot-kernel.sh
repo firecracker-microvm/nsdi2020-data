df5a152f80fc859a7040e593caf1dc407f3fc01a
Rolf Neugebauer <rn@rneugeba.io>
2019-08-19 20:37:15 +0100

    scripts: Restructure run-boot-kernel.sh
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-boot-kernel.sh
+++ scripts/run-boot-kernel.sh
@@ -22,40 +22,30 @@ done
 RAW=${DIR}/raw
 mkdir -p ${RAW}
 
-PCI_DAT=${RAW}/boot-serial-fc-pci.dat
-PCI_CDF=${DIR}/boot-serial-fc-pci-cdf.dat
+run_firecracker() {
+    local KERNEL=$1
+    local PRE=$2
 
-LK_DAT=${RAW}/boot-serial-fc-linuxkit.dat
-LK_CDF=${DIR}/boot-serial-fc-linuxkit-cdf.dat
+    local DAT=${RAW}/${PRE}.dat
+    local CDF=${DIR}/${PRE}-cdf.dat
 
-rm -f ${PCI_DAT} ${PCI_CDF} ${LK_DAT} ${LK_CDF}
-
-killall -9 firecracker 2> /dev/null
-for i in $(seq ${ITER}); do
-    echo "Firecracker+PCI kernel: $i"
-    ./util_start_fc.sh -b ../bin/firecracker \
-                  -k ../img/boot-time-pci-vmlinux \
-                  -r ../img/boot-time-disk.img \
-                  -c $CORES \
-                  -m $MEM \
-                  -t ${PCI_DAT}
-    sleep 0.4
+    rm -f ${DAT} ${CDF}
+    
     killall -9 firecracker 2> /dev/null
-done
-rm -f *.log
-./util_gen_cdf.py ${PCI_DAT} ${PCI_CDF}
+    for i in $(seq ${ITER}); do
+        echo "Firecracker+PCI kernel: $i"
+        ./util_start_fc.sh -b ../bin/firecracker \
+                           -k ../img/${KERNEL} \
+                           -r ../img/boot-time-disk.img \
+                           -c $CORES \
+                           -m $MEM \
+                           -t ${DAT}
+        sleep 1
+        killall -9 firecracker 2> /dev/null
+    done
+    rm -f *.log
+    ./util_gen_cdf.py ${DAT} ${CDF}
+}
 
-killall -9 firecracker 2> /dev/null
-for i in $(seq ${ITER}); do
-    echo "Firecracker+LinuxKit kernel: $i"
-    ./util_start_fc.sh -b ../bin/firecracker \
-                  -k ../img/boot-time-linuxkit-vmlinux \
-                  -r ../img/boot-time-disk.img \
-                  -c $CORES \
-                  -m $MEM \
-                  -t ${LK_DAT}
-    sleep 0.4
-    killall -9 firecracker 2> /dev/null
-done
-rm -f *.log
-./util_gen_cdf.py ${LK_DAT} ${LK_CDF}
+run_firecracker ../img/boot-time-vmlinux boot-serial-fc-nopci
+run_firecracker ../img/boot-time-linuxkit-vmlinux boot-serial-fc-linuxkit
