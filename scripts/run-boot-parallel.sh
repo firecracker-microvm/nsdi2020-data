b6b9251f7c78e6b3d4c4fae57e37c9f514ff74fc
Rolf Neugebauer <rn@rneugeba.io>
2019-08-20 00:25:13 +0100

    scripts: Tweak parallel script
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-boot-parallel.sh
+++ scripts/run-boot-parallel.sh
@@ -26,29 +26,33 @@ RAW=${DIR}/raw
 mkdir -p ${RAW}
 
 run_firecracker() {
-    FC_DAT=${RAW}/boot-parallel-${PARALLEL}-fc.dat
-    FC_CDF=${DIR}/boot-parallel-${PARALLEL}-fc-cdf.dat
+    local DAT=${RAW}/boot-parallel-${PARALLEL}-fc.dat
+    local CDF=${DIR}/boot-parallel-${PARALLEL}-fc-cdf.dat
 
-    rm -f ${FC_DAT} ${FC_CDF}
+    rm -f ${DAT} ${CDF}
+
+    echo "Firecracker"
 
     killall -9 firecracker 2> /dev/null
     seq 1 $ITER | xargs -L1 -P$PARALLEL ./util_start_fc.sh -b ../bin/firecracker \
-                        -k ../img/boot-time-vmlinux \
+                        -k ../img/boot-time-pci-vmlinux \
                         -r ../img/boot-time-disk.img \
                         -c $CORES \
                         -m $MEM \
-                        -t ${FC_DAT}
+                        -t ${DAT}
     sleep 10
     killall -9 firecracker 2> /dev/null
     rm -f *.log
-    ./util_gen_cdf.py ${FC_DAT} ${FC_CDF}
+    ./util_gen_cdf.py ${DAT} ${CDF}
 }
 
 run_cloudhv() {
-    CHV_DAT=${RAW}/boot-parallel-${PARALLEL}-chv.dat
-    CHV_CDF=${DIR}/boot-parallel-${PARALLEL}-chv-cdf.dat
+    local DAT=${RAW}/boot-parallel-${PARALLEL}-chv.dat
+    local CDF=${DIR}/boot-parallel-${PARALLEL}-chv-cdf.dat
 
-    rm -f ${CHV_DAT} ${CHV_CDF}
+    rm -f ${DAT} ${CDF}
+
+    echo "Cloud Hypervisor"
 
     killall -9 cloud-hypervisor 2> /dev/null
     seq 1 $ITER | xargs -L1 -P$PARALLEL ./util_start_cloudhv.sh -b ../bin/cloud-hypervisor \
@@ -56,31 +60,32 @@ run_cloudhv() {
                         -r ../img/boot-time-disk.img \
                         -c $CORES \
                         -m $MEM \
-                        -t ${CHV_DAT}
+                        -t ${DAT} > /dev/null
     sleep 10
     killall -9 cloud-hypervisor 2> /dev/null
     rm -f *.log
-    ./util_gen_cdf.py ${CHV_DAT} ${CHV_CDF}
+    ./util_gen_cdf.py ${DAT} ${CDF}
 }
 
 run_qemu() {
-    QEMU_DAT=${RAW}/boot-parallel-${PARALLEL}-qemu.dat
-    QEMU_CDF=${DIR}/boot-parallel-${PARALLEL}-qemu-cdf.dat
+    local DAT=${RAW}/boot-parallel-${PARALLEL}-qemu.dat
+    local CDF=${DIR}/boot-parallel-${PARALLEL}-qemu-cdf.dat
 
-    rm -f ${QEMU_DAT} ${QEMU_CDF}
+    rm -f ${DAT} ${CDF}
 
-    killall -9 qemu-system-x86_64 2> /dev/null
+    echo "Qemu+qboot"
+
     seq 1 $ITER | xargs -L1 -P$PARALLEL  ./util_start_qemu.sh -b ../bin/qemu-system-x86_64 \
                         -k ../img/boot-time-pci-vmlinuz \
                         -r ../img/boot-time-disk.img \
                         -w qboot.bin \
                         -c $CORES \
                         -m $MEM \
-                        -t ${QEMU_DAT}
+                        -t ${DAT}
     sleep 10
     killall -9 qemu-system-x86_64 2> /dev/null
     rm -f *.log
-    ./util_gen_cdf.py ${QEMU_DAT} ${QEMU_CDF}
+    ./util_gen_cdf.py ${DAT} ${CDF}
 }
 
 run_firecracker
