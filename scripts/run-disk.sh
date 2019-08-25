090c95756a102f935217f65aab4dd7227af23bad
Rolf Neugebauer <rn@rneugeba.io>
2019-08-25 22:22:30 +0100

    scripts: Addc loud hypervisor to disk script
    and restructure it a little
    
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/run-disk.sh
+++ scripts/run-disk.sh
@@ -23,66 +23,87 @@ TESTS="$TESTS rand-write-128k rand-write-128k-qd1"
 RAW=${DIR}/raw
 mkdir -p ${RAW}
 
-FC_PRE=fio-fc
-QEMU_PRE=fio-qemu
-
 # Create a file if it does not exist
 [ -e $FILE ] || fallocate -l 10G $FILE
 
 killall -9 firecracker 2> /dev/null
+killall -9 cloud-hypervisor 2> /dev/null
 killall -9 qemu-system-x86_64 2> /dev/null
 
 ID=0
 VM_IP=$(./util_ipam.sh -v $ID)
 SSH="ssh -i ../etc/ssh-bench.key -F ../etc/ssh-config root@${VM_IP}"
 
-echo "Firecracker: Starting"
-./util_start_fc.sh \
-    -b ../bin/firecracker \
-    -k ../img/bench-ssh-vmlinux \
-    -r ../img/bench-ssh-disk.img \
-    -c $CORES \
-    -m $MEM \
-    -i $ID \
-    -n \
-    -f $FILE \
-    &
+run_firecracker() {
+    local PRE=fio-fc
+    echo "Firecracker: Starting"
+    ./util_start_fc.sh -b ../bin/firecracker \
+        -k ../img/bench-ssh-vmlinux -r ../img/bench-ssh-disk.img \
+        -c $CORES -m $MEM -i $ID \
+        -n -f $FILE &
 
-# 10 seconds should be enough
-sleep 10
+    # 10 seconds should be enough
+    sleep 10
 
-# Copy config
-cat ../etc/fio-vm.cfg | ${SSH} "cat > fio-vm.cfg"
+    # Copy config
+    cat ../etc/fio-vm.cfg | ${SSH} "cat > fio-vm.cfg"
 
-for TEST in $TESTS; do
-    echo "Running $TEST"
-    ${SSH} "fio --output-format=json --output=$FC_PRE-$TEST.json --section=$TEST fio-vm.cfg"
-    ${SSH} "cat $FC_PRE-$TEST.json" > ${RAW}/${FC_PRE}-${TEST}.json
-done
-killall -9 firecracker 2> /dev/null
+    for TEST in $TESTS; do
+        echo "Running $TEST"
+        ${SSH} "fio --output-format=json --output=$PRE-$TEST.json --section=$TEST fio-vm.cfg"
+        ${SSH} "cat $PRE-$TEST.json" > ${RAW}/${PRE}-${TEST}.json
+    done
 
-sleep 10
-./util_start_qemu.sh \
-    -b ../bin/qemu-system-x86_64 \
-    -k ../img/bench-ssh-vmlinuz \
-    -r ../img/bench-ssh-disk.img \
-    -w qboot.bin \
-    -c $CORES \
-    -m $MEM \
-    -i $ID \
-    -n \
-    -f $FILE \
-    &
+    killall -9 firecracker 2> /dev/null
+}
 
-# 10 seconds should be enough
-sleep 10
+run_cloudhv() {
+    local PRE=fio-chv
+    echo "Cloud Hypervisor: Starting"
+    ./util_start_cloudhv.sh -b ../bin/cloud-hypervisor \
+        -k ../img/bench-ssh-vmlinux -r ../img/bench-ssh-disk.img \
+        -c $CORES -m $MEM -i $ID \
+        -n -f $FILE &
 
-# Copy config
-cat ../etc/fio-vm.cfg | ${SSH} "cat > fio-vm.cfg"
+    # 10 seconds should be enough
+    sleep 10
 
-for TEST in $TESTS; do
-    echo "Running $TEST"
-    ${SSH} "fio --output-format=json --output=$QEMU_PRE-$TEST.json --section=$TEST fio-vm.cfg"
-    ${SSH} "cat $QEMU_PRE-$TEST.json" > ${RAW}/${QEMU_PRE}-${TEST}.json
-done
-killall -9 qemu-system-x86_64 2> /dev/null
+    # Copy config
+    cat ../etc/fio-vm.cfg | ${SSH} "cat > fio-vm.cfg"
+
+    for TEST in $TESTS; do
+        echo "Running $TEST"
+        ${SSH} "fio --output-format=json --output=$PRE-$TEST.json --section=$TEST fio-vm.cfg"
+        ${SSH} "cat $PRE-$TEST.json" > ${RAW}/${PRE}-${TEST}.json
+    done
+
+    killall -9 cloud-hypervisor 2> /dev/null
+}
+
+run_qemu() {
+    local PRE=fio-qemu
+    echo "qemu: Starting"
+    ./util_start_qemu.sh -b ../bin/qemu-system-x86_64 \
+    -k ../img/bench-ssh-vmlinuz -r ../img/bench-ssh-disk.img -w qboot.bin \
+    -c $CORES -m $MEM -i $ID \
+    -n -f $FILE &
+
+    # 10 seconds should be enough
+    sleep 10
+
+    # Copy config
+    cat ../etc/fio-vm.cfg | ${SSH} "cat > fio-vm.cfg"
+
+    for TEST in $TESTS; do
+        echo "Running $TEST"
+        ${SSH} "fio --output-format=json --output=$PRE-$TEST.json --section=$TEST fio-vm.cfg"
+        ${SSH} "cat $PRE-$TEST.json" > ${RAW}/${PRE}-${TEST}.json
+    done
+    killall -9 qemu-system-x86_64 2> /dev/null
+}
+
+run_firecracker
+sleep 5
+run_cloudhv
+sleep 5
+run_qemu
