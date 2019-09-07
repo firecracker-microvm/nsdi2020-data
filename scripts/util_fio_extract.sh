9c33216584f8bd745131b31555df4340831c699f
Rolf Neugebauer <rn@rneugeba.io>
2019-09-07 10:25:15 +0100

    scripts: Improve fio extraction script
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- scripts/util_fio_extract.sh
+++ scripts/util_fio_extract.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-
 while [ $# -gt 0 ]; do
     case $1 in
         -d) shift; DIR=$1
@@ -13,6 +12,8 @@ TESTS="rand-read-4k rand-read-4k-qd1"
 TESTS="$TESTS rand-write-4k rand-write-4k-qd1"
 TESTS="$TESTS rand-read-128k rand-read-128k-qd1"
 TESTS="$TESTS rand-write-128k rand-write-128k-qd1"
+
+CONFIGS="fio-fc fio-chv fio-qemu fio-metal"
 
 RAW=${DIR}/raw
 
@@ -30,50 +31,50 @@ process() {
 
     case $bm in
         rand-read-4k)
-            local test="read_4k"
+            local test="4k read"
             local iops=$r_iops
             local bw=$r_bw
             local lat99=$r_lat99
             ;;
         rand-read-4k-qd1)
-            local test="read_4k_qd1"
+            local test="4k read (qd1)"
             local iops=$r_iops
             local bw=$r_bw
             local lat99=$r_lat99
             ;;
         rand-read-128k)
-            local test="read_128k"
+            local test="128k read"
             local iops=$r_iops
             local bw=$r_bw
             local lat99=$r_lat99
             ;;
         rand-read-128k-qd1)
-            local test="read_128k_qd1"
+            local test="128k read (qd1)"
             local iops=$r_iops
             local bw=$r_bw
             local lat99=$r_lat99
             ;;
 
         rand-write-4k)
-            local test="write_4k"
+            local test="4k write"
             local iops=$w_iops
             local bw=$w_bw
             local lat99=$w_lat99
-            ;;
+            ;; 
         rand-write-4k-qd1)
-            local test="write_4k_qd1"
+            local test="4k write (qd1)"
             local iops=$w_iops
             local bw=$w_bw
             local lat99=$w_lat99
             ;;
         rand-write-128k)
-            local test="write_128k"
+            local test="128k write"
             local iops=$w_iops
             local bw=$w_bw
             local lat99=$w_lat99
             ;;
         rand-write-128k-qd1)
-            local test="write_128k_qd1"
+            local test="128k write (qd1)"
             local iops=$w_iops
             local bw=$w_bw
             local lat99=$w_lat99
@@ -83,34 +84,18 @@ process() {
             exit 1
     esac
 
-    echo "$test $iops $bw $lat99" >> $out
+    echo "\"$test\" $iops $bw $lat99" >> $out
 }
 
+for CONFIG in $CONFIGS; do
+    OUT=${DIR}/${CONFIG}.dat
+    PRE=${RAW}/${CONFIG}
 
-PRE=fio-fc
-OUT=${DIR}/${PRE}.dat
-echo "# Benchmark iops bw lat99" > $OUT
-for TEST in $TESTS; do
-    process $TEST ${RAW}/${PRE}-${TEST}.json $OUT
-done
+    # skip if one data file does not exist
+    [ -f ${PRE}-rand-read-4k.json ] || continue
 
-PRE=fio-chv
-OUT=${DIR}/${PRE}.dat
-echo "# Benchmark iops bw lat99" > $OUT
-for TEST in $TESTS; do
-    process $TEST ${RAW}/${PRE}-${TEST}.json $OUT
-done
-
-PRE=fio-qemu
-OUT=${DIR}/${PRE}.dat
-echo "# Benchmark iops bw lat99" > $OUT
-for TEST in $TESTS; do
-    process $TEST ${RAW}/${PRE}-${TEST}.json $OUT
-done
-
-PRE=fio-metal
-OUT=${DIR}/${PRE}.dat
-echo "# Benchmark iops bw lat99" > $OUT
-for TEST in $TESTS; do
-    process $TEST ${RAW}/${PRE}-${TEST}.json $OUT
+    echo "# Benchmark iops bw lat99" > $OUT
+    for TEST in $TESTS; do
+        process ${TEST} ${PRE}-${TEST}.json ${OUT}
+    done
 done
